f2b4a27cf7a2d39ed7c6deb125b0c0320bf9182c
Romrom8434 <romainvian13000@gmail.com>
2026-08-06 18:52:42 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+// SW auto-destruction — se désinstalle immédiatement
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+  );
+});
